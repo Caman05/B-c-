097bbb091b06c8af9c4357ddcf67f43973271ce0
Caman05 <camanth5@gmail.com>
2026-09-13 10:43:55 +0700

    Update mockMusic.ts

--- src/data/mockMusic.ts
+++ src/data/mockMusic.ts
@@ -5,7 +5,7 @@ export const PLAYLIST: Track[] = [
     id: 'track-1',
     title: 'Wo Ai Ni',
     artist: 'Trash Dee',
-    audioUrl: '/audio/wo-ai-ni.mp3',
+    audioUrl: 'public/storage/music/Giot-Le-Thien-Su-mp3',
     isActive: true,
     duration: 118,
     rootFreq: 220,
